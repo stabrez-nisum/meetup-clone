5f3fb81d627f8d64092278e4a034eb30443afaee
Harika Kottapalli <hkottapalli@nisum.com>
2020-11-16 16:17:05 +0530

    integrated bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <signup></signup>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Signup from './components/Signup.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Signup
   }
 }
 </script>
---
+++ src/components/Signup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+     <h2>Hello World</h2> 
+     <button class="btn btn-sm btn-primary">Sign up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Signup"
+};
+</script>
+
+<style scoped>
+</style>
