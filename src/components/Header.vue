5b22ae29cf2ca4ad95a259393e6db0498ff3b544
Shaik Tabrez <stabrez@nisum.com>
2020-11-16 18:15:21 +0530

    Add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" >
+      <b-navbar-brand href="#/home">
+        <img src="../../public/meetup-logo.svg" alt="Meetup" class="logo">
+      </b-navbar-brand>
+      
+      <b-collapse id="nav-collapse" is-nav>        
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#/login"><strong>Login</strong></b-nav-item>
+          <b-nav-item href="#/signup"><strong>Signup</strong></b-nav-item>         
+        </b-navbar-nav>
+      </b-collapse>
+
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",  
+  mounted() {
+    // Init operations
+  },
+};
+</script>
+<style scoped>
+.logo{
+    height: 50px;
+    width: 150px;
+}
+</style>
