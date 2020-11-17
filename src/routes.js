03e15bac0d8907501aee8e483f9c38711b4620e5
Shaik Tabrez <stabrez@nisum.com>
2020-11-17 10:50:16 +0530

    Add redirect

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ import Home from './components/Home.vue';
 
 export const routes = [
     {path: '/', name:'Home', component: Home },
+    {path: '/home', redirect: '/'},
     {path: '/signup', name:'Signup', component: Signup},
 ];
