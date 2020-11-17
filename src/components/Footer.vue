22f6e912f18a9a6d050106a8b9750476e52b92a1
Shaik Tabrez <stabrez@nisum.com>
2020-11-17 10:50:33 +0530

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fluid-container footer">
+    <p class="text-center">
+      Copyright &copy; 2020, Nisum. All Rights Reserved.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
