c60fe1a31e3ffc75db9b186231ff038d9cc3fe6b
Shaik Tabrez <stabrez@nisum.com>
2020-11-19 00:10:37 +0530

    Add login search bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,22 +4,33 @@
       <b-navbar-brand href="/">
         <img src="../../public/meetup-logo.svg" alt="Meetup" class="logo" />
       </b-navbar-brand>
-      
+
       <b-collapse v-if="isUser" id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-						<b-button class="mr-1" variant="danger" pill>T</b-button>
-            <strong>Tabrez</strong>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <b-button class="mr-1" variant="danger" pill>T</b-button>
+              <strong>Tabrez</strong>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
 
       <b-collapse v-else id="nav-collapse" is-nav>
+        <div class="d-flex justify-content-center">
+          <b-navbar>
+            <b-nav-form>
+              <b-form-input
+                class="mr-sm-2 header-search"
+                placeholder="Search Events"
+              ></b-form-input>
+            </b-nav-form>
+          </b-navbar>
+        </div>
+
         <b-navbar-nav class="ml-auto">
           <b-nav-item href="/login"><strong>Login</strong></b-nav-item>
           <b-nav-item href="/signup"><strong>Signup</strong></b-nav-item>
@@ -30,8 +41,12 @@
 </template>
 
 <script>
+// import Search from './Search.vue'
 export default {
   name: "Header",
+  components: {
+    // Search
+  },
   mounted() {
     // Init operations
   },
@@ -47,5 +62,8 @@ export default {
 .logo {
   height: 50px;
   width: 150px;
+}
+.header-search{
+  width: 500px!important;
 }
 </style>
