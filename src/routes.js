b0de43cde74b49a950f0271136e6ee59f80725cc
Shaik Tabrez <stabrez@nisum.com>
2020-11-18 10:00:10 +0530

    Add dynamic id to router for events

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Signup from './components/Signup.vue';
 import Home from './components/Home.vue';
 import Login from './components/Login.vue';
+import Event from './components/Event.vue'
 
 export const routes = [
     {path: '/', name:'Home', component: Home },
     {path: '/home', redirect: '/'},
     {path: '/signup', name:'Signup', component: Signup},
     {path: '/login', name:'Login', component: Login},
+    { path: '/event/:id', component: Event }
 ];
